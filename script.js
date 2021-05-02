70005190ce619d2f1d840fe43ded2fb1b9737cc5
Roman Polek <polekroman@gmail.com>
2021-05-02 03:02:53 +0100

    capturing screen

---
+++ script.js
@@ -0,0 +1,51 @@
+function break_page() {
+    console.log("breaking page")
+
+    var overlay = document.getElementById("page_breaker_overlay")
+    if(overlay == null) {
+        overlay = document.createElement("div")
+        overlay.setAttribute("data-html2canvas-ignore", "true")
+        overlay.id = "page_breaker_overlay"
+        overlay.style.position = "fixed"
+        overlay.style.top = "0px"
+        overlay.style.left = "0px"
+        overlay.style.zIndex= "999"
+        
+
+        //create close button
+        var close_button = document.createElement("p")
+        close_button.onclick = () => {overlay.remove()}
+        close_button.innerHTML = "X"
+        close_button.style.cursor = "pointer"
+        close_button.style.fontFamily = "Arial"
+        close_button.style.padding = "10px"
+        close_button.style.margin = "20px"
+        close_button.style.fontSize = "2em"
+        close_button.style.color = "white"
+        close_button.style.borderRadius = "800px"
+        close_button.style.backgroundColor = "brown"
+        close_button.style.display = "inline-block"
+        close_button.style.width = "30px"
+        close_button.style.height = "30px"
+        close_button.style.textAlign = "center"
+        close_button.style.position = "fixed"
+        close_button.style.top = "0px"
+        close_button.style.right = "0px"
+        close_button.style.zIndex= "999"
+        overlay.appendChild(close_button)
+
+        document.body.appendChild(overlay)
+    }
+
+
+    var canvas = document.getElementById("page_breaker_canvas")
+    if(canvas != null) {
+        canvas.remove()
+    }
+    
+    html2canvas(document.body).then((c) => {
+        var canvas = c
+        canvas.id="page_breaker_canvas"
+        overlay.appendChild(canvas)
+    })
+}
